8dd949da1475b551dbce3a924345e1167645e950
Weixiong Zheng <wzheng@carbon3d.com>
2021-04-11 11:56:20 -0700

    add unit test lib

---
+++ testing/testing_main.cc
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+
+// TODO: Improve the naive main function below
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
