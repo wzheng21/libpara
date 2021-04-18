e543fed2df1ca4c11d3422e64959cf1751617500
Victor Zheng <zwxne2010@gmail.com>
2021-04-17 19:54:28 -0700

    remove bazel facilities and change folder structure

--- testing/testing_main.cc
+++
@@ -1,7 +0,0 @@
-#include "gtest/gtest.h"
-
-// TODO: Improve the naive main function below
-int main(int argc, char *argv[]) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
