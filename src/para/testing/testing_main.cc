8b16da379e273c2e575cd0bb040632b52138b389
Victor Zheng <zwxne2010@gmail.com>
2021-04-24 03:05:40 -0700

    add prctl in testing_main

--- src/para/testing/testing_main.cc
+++ src/para/testing/testing_main.cc
@@ -1,9 +1,15 @@
 #include "gtest/gtest.h"
 #include "glog/logging.h"
 
+#include <signal.h>
+#include <sys/prctl.h>
+
 // TODO: Improve the naive main function below
 int main(int argc, char *argv[]) {
   google::InitGoogleLogging(argv[0]);
   testing::InitGoogleTest(&argc, argv);
+  if (prctl(PR_SET_PDEATHSIG, SIGKILL) < 0) {
+    PLOG(ERROR) << "prctl(PR_SET_PDEATHSIG, SIGKILL) failed";
+  }
   return RUN_ALL_TESTS();
 }
