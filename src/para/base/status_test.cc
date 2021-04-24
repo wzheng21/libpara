8d3a63acd08c094795901acdfe8b0b1cd69a438a
Victor Zheng <zwxne2010@gmail.com>
2021-04-24 13:18:52 -0700

    added copyright headers

--- src/para/base/status_test.cc
+++ src/para/base/status_test.cc
@@ -1,3 +1,9 @@
+// Copyright (c) 2021
+// Authors: Weixiong (Victor) Zheng
+// All rights reserved
+//
+// SPDX-License-Identifier: MIT
+
 #include "para/base/status.h"
 #include "para/testing/testing.h"
 
